d736909c3102518d5b446e40267aa64ee9e12129
Dasha Turevich <dasha.turevich@yandex.ru>
2023-05-22 12:43:42 +0300

    add test for project JenkinsTestQA POM

--- src/test/java/school/redrover/FooterJenkinsVersionTest.java
+++ src/test/java/school/redrover/FooterJenkinsVersionTest.java
@@ -11,8 +11,7 @@ import school.redrover.runner.BaseTest;
 import static org.openqa.selenium.support.ui.ExpectedConditions.numberOfWindowsToBe;
 
 public class FooterJenkinsVersionTest extends BaseTest {
-    private final String expectedJenkinsVersion = "Jenkins 2.387.2";
-    private final String expectedSiteTitle = "Jenkins";
+
     @Test
     public void testFooterJenkinsVersion() {
         WebElement linkVersion = getDriver().findElement(By.xpath("//a[text()='Jenkins 2.387.2']"));
@@ -38,27 +37,5 @@ public class FooterJenkinsVersionTest extends BaseTest {
         Assert.assertEquals(brandJenkins.getText(), "Jenkins");
     }
 
-    @Test
-    public void testFooterJenkinsVersionOnNodesPage() {
-        getDriver().findElement(By.xpath("//span[@class='pane-header-title']/a")).click();
-        WebElement jenkinsVersion = getDriver().findElement(By.xpath("//a[@target='_blank']"));
-        String actualJenkinsVersion = jenkinsVersion.getText();
 
-        Assert.assertEquals(actualJenkinsVersion, expectedJenkinsVersion, "Jenkins version does not match");
-    }
-
-    @Ignore
-    @Test
-    public void testClickOnJenkinsVersionOpensSiteOnNodesPage(){
-
-        getDriver().findElement(By.xpath("//span[@class='pane-header-title']/a")).click();
-        getDriver().findElement(By.xpath("//a[@target='_blank']")).click();
-
-        for(String winHandle : getDriver().getWindowHandles()) {
-            getDriver().switchTo().window(winHandle);
-        }
-
-        String actualSiteTitle = getDriver().findElement(By.xpath("//h1[@class='page-title']/span")).getText();
-        Assert.assertEquals(actualSiteTitle, expectedSiteTitle);
-    }
 }
